1eaf01684ef6b413e22538ff304840534f426b82
uktweeter19 <uktweeter19@gmail.com>
2026-04-15 16:30:46 -0400

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,13 @@
+4:29 PM Kevin Deatherage - 1114: 
+4:30 PM Kevin Deatherage - 1114: export const metadata = {
+  title: 'JARVIS',
+  description: 'Family Intelligence System',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
