062db16ae6a6e1f7516c809f661e654c13a7c54e
uktweeter19 <uktweeter19@gmail.com>
2026-04-15 16:38:22 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
-4:29 PM Kevin Deatherage - 1114: 
-4:30 PM Kevin Deatherage - 1114: export const metadata = {
+export const metadata = {
   title: 'JARVIS',
   description: 'Family Intelligence System',
 }
